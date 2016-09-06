67f8833e3db773f6d24614a104f4b2dd6091ab1c
Jeremiah Orem <oremj@oremj.com>
2016-09-06 13:50:06 -0700

    print stats on interval

--- autoscaler/dynamodb.go
+++ autoscaler/dynamodb.go
@@ -2,12 +2,15 @@ package autoscaler
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
+
+var LookBackDuration = -5 * time.Minute
 
 type DynamoTable struct {
 	TableName string
@@ -24,8 +27,59 @@ func NewDynamoTable(tableName string, awsSession *session.Session) *DynamoTable 
 	}
 }
 
+func (d *DynamoTable) checkTable() error {
+	readConsumed, err := d.ReadUnitsConsumed()
+	if err != nil {
+		return fmt.Errorf("ReadUnitsConsumed: %v", err)
+	}
+
+	readProvisioned, err := d.ReadUnitsProvisioned()
+	if err != nil {
+		return fmt.Errorf("ReadUnitsProvisioned: %v", err)
+	}
+
+	writeConsumed, err := d.WriteUnitsConsumed()
+	if err != nil {
+		return fmt.Errorf("WriteUnitsConsumed: %v", err)
+	}
+
+	writeProvisioned, err := d.WriteUnitsProvisioned()
+	if err != nil {
+		return fmt.Errorf("WriteUnitsProvisioned: %v", err)
+	}
+
+	writeThrottled, err := d.WriteThrottledEvents()
+	if err != nil {
+		return fmt.Errorf("WriteThrottleEvents: %v", err)
+	}
+
+	readThrottled, err := d.ReadThrottledEvents()
+	if err != nil {
+		return fmt.Errorf("ReadThrottleEvents: %v", err)
+	}
+
+	fmt.Println(readConsumed, readProvisioned, writeConsumed, writeProvisioned, writeThrottled, readThrottled)
+
+	return nil
+}
+
+func (d *DynamoTable) Monitor(freq time.Duration) {
+	ticker := time.Tick(freq)
+	for range ticker {
+		// If the check fails to return after two intervals, panic
+		timer := time.AfterFunc(2*freq, func() {
+			panic("Deadlock detected.")
+		})
+		log.Println("Checking Table: ", d.TableName)
+		if err := d.checkTable(); err != nil {
+			log.Printf("checkTable: %v", err)
+		}
+		timer.Stop()
+	}
+}
+
 func (d *DynamoTable) ReadUnitsConsumed() (int64, error) {
-	return d.CloudWatch.ConsumedReadCapacityUnits(d.TableName, time.Now().Add(-20*time.Minute))
+	return d.CloudWatch.ConsumedReadCapacityUnits(d.TableName, time.Now().Add(LookBackDuration))
 }
 
 func (d *DynamoTable) ReadUnitsProvisioned() (int64, error) {
@@ -38,7 +92,7 @@ func (d *DynamoTable) ReadUnitsProvisioned() (int64, error) {
 }
 
 func (d *DynamoTable) WriteUnitsConsumed() (int64, error) {
-	return d.CloudWatch.ConsumedWriteCapacityUnits(d.TableName, time.Now().Add(-20*time.Minute))
+	return d.CloudWatch.ConsumedWriteCapacityUnits(d.TableName, time.Now().Add(LookBackDuration))
 }
 
 func (d *DynamoTable) WriteUnitsProvisioned() (int64, error) {
@@ -51,11 +105,11 @@ func (d *DynamoTable) WriteUnitsProvisioned() (int64, error) {
 }
 
 func (d *DynamoTable) ReadThrottledEvents() (int64, error) {
-	return d.CloudWatch.ReadThrottleEvents(d.TableName, time.Now().Add(-20*time.Minute))
+	return d.CloudWatch.ReadThrottleEvents(d.TableName, time.Now().Add(LookBackDuration))
 }
 
 func (d *DynamoTable) WriteThrottledEvents() (int64, error) {
-	return d.CloudWatch.WriteThrottleEvents(d.TableName, time.Now().Add(-20*time.Minute))
+	return d.CloudWatch.WriteThrottleEvents(d.TableName, time.Now().Add(LookBackDuration))
 }
 
 func (d *DynamoTable) describeTable() (*dynamodb.TableDescription, error) {
--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/oremj/dynamodb-autoscaler/autoscaler"
 )
 
+var awsRegion = flag.String("region", "us-east-1", "The region where the table is located.")
 var tableName = flag.String("table", "", "The table to monitor.")
 
 func main() {
@@ -18,25 +21,18 @@ func main() {
 		fmt.Println("-table must be set")
 		os.Exit(1)
 	}
-	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-1")})
-	if err != nil {
-		panic(err)
-	}
-	cw := autoscaler.NewDynamoTable(*tableName, sess)
-	consumedRead, err := cw.ReadUnitsConsumed()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(consumedRead / 5.0)
-	consumedRead, err = cw.ReadUnitsProvisioned()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(consumedRead)
 
-	throttled, err := cw.WriteThrottledEvents()
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(*awsRegion)})
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(throttled)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		dt := autoscaler.NewDynamoTable(*tableName, sess)
+		dt.Monitor(5 * time.Second)
+	}()
+	wg.Wait()
 }
